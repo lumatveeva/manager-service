c8ff61bbe140042176ebcdd971807eb9638bb344
Liubov Matveeva <mtvliuba@gmail.com>
2025-05-08 21:07:26 +0500

    init

---
+++ src/main/java/ru/lumat/managerservice/ManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.lumat.managerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lumat/managerservice/ManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lumat.managerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
